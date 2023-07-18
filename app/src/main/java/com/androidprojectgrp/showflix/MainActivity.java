2c4bef0d2f4ef88d1111fcd970c15ae5b6dacc8c
Infernopeace <infernoroger818@gmail.com>
2023-07-18 12:43:46 +0530

    ShowFlix.v2

--- app/src/main/java/com/androidprojectgrp/showflix/MainActivity.java
+++ app/src/main/java/com/androidprojectgrp/showflix/MainActivity.java
@@ -1,8 +1,12 @@
 package com.androidprojectgrp.showflix;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.optmenu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemid= item.getItemId();
+
+        if (itemid == R.id.home) {
+            Toast.makeText(MainActivity.this, "home", Toast.LENGTH_SHORT).show();
+        } else if (itemid == R.id.profile) {
+            Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+        }else if (itemid == R.id.logout){
+            Toast.makeText(MainActivity.this, "logout", Toast.LENGTH_SHORT).show();
+        }else{
+            return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
